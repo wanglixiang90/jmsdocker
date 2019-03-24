99359058883dbc7c1ce33eee602b8a6c0ae6cbba
DESKTOP-KDQ0IHT\admin <15201376500@163.com>
2019-03-24 13:41:00 +0800

    .

--- coco-run/start-jms-coco.sh
+++ coco-run/start-jms-coco.sh
@@ -6,6 +6,4 @@ docker run -d --name jms_coco \
 -v ${basedir}/logs:/opt/coco/logs \
 -p 5000:5000 -p 3222:2222 \
 --add-host jumpserver:192.168.2.55 \
---add-host jms_mysql:192.168.2.55 \
---add-host jms_redis:192.168.2.55 \
 jms/coco
